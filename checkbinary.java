79b15b9519b64d35d49b79eec348bae6e1e14d31
kriti3113 <53795419+kriti3113@users.noreply.github.com>
2019-11-21 02:46:06 +0530

    Add files via upload

---
+++ checkbinary.java
@@ -0,0 +1,78 @@
+import java.util.Scanner;
+class Node
+{
+  int data;
+  Node left,right;
+  Node(int d)
+  {
+    data=d;
+    left=right=null;
+  }
+}
+class chkbst
+{
+  static Node root;
+  chkbst()
+  {
+    root=null;
+  }
+
+   public static void preOrder(Node root)
+    {
+    if(root!=null)
+      {
+        System.out.println(root.data);
+        preOrder(root.left);
+        preOrder(root.right);
+      }
+      return;
+    }
+public static boolean chkb(Node root,Node prev)
+{
+
+  //Node par=null;
+  if(root!=null)
+ {
+  if(root.left!=null)
+   {
+  chkb(root.left,root);
+  System.out.println(root.data);
+  if(root.left.data>root.data)
+    {
+    return false;
+    }
+  }
+ if(root.right!=null)
+  {
+  chkb(root.right,root);
+  if(root.right.data<root.data)
+   {
+    return false;
+   }
+  }
+}
+  return true;
+}
+public static void main(String[] args) {
+  chkbst b=new chkbst();
+  Scanner sc=new Scanner(System.in);
+  
+  root = new Node(19); 
+    root.left = new Node(8); 
+    root.left.left = new Node(7); 
+    root.right = new Node(22); 
+    root.right.left = new Node(21); 
+    root.right.right = new Node(25);
+    //preOrder(root);
+  boolean c=chkb(root,null);
+  if(c==true)
+  {
+    System.out.println("The given tree is a bst");
+  }
+  else
+  {
+    System.out.println("The given tree is not a bst");
+  }
+}}
+
+    
