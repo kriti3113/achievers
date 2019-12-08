5443cfe4e46889582a24716f698d2226de08dcf7
kriti3113 <53795419+kriti3113@users.noreply.github.com>
2019-12-08 11:56:37 +0530

    Update printleafnodes.java

--- printleafnodes.java
+++ printleafnodes.java
@@ -35,35 +35,20 @@ class printleafnodes
 		}
        return root;
 	}
-	public static void printhighestlevel(Node root,int level)
-	{
-		if(root==null)
-			return;
-      if(level==1)
-      	System.out.print(root.data+" ");
-      else if(level>1)
-      {
-      	printhighestlevel(root.left,level-1);
-      	printhighestlevel(root.right,level-1);
-      }
-	}
-
-    public static int height(Node root)
+    public static void printlfnodes(Node root)
     {
-    	//int l=0,r=0;
     	if(root==null)
-    		return 0;
-        
-        int l= height(root.left);
-        int r= height(root.right);
-       if(l>r)
-       	return (1+l);
-       else
-       	return (1+r);
-       
+    		return;
+        else if(root.left==null&&root.right==null)
+    		System.out.print(root.data+" ");
+    	else
+    	{
+    		printlfnodes(root.left);
+    		printlfnodes(root.right);
+    	}
     }
 	public static void main(String[] args) {
-		printleafnodes p=new printleafnodes();
+		printleafnodes tree=new printleafnodes();
 		Scanner sc=new Scanner(System.in);
 		System.out.println("Enter the number of elements");
 		int n=sc.nextInt();
@@ -71,9 +56,8 @@ class printleafnodes
 		for(int i=0;i<n;i++)
 		{
 			int data=sc.nextInt();
-		p.insert(data);
+	       insert(data);
 	    }
-		int h=height(root);
-        printhighestlevel(root,h);
+      printlfnodes(root);
 	}
 }
